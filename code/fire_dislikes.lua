1af8907d7472c0ea48ae22e8328b333ad1b4571d
SnoillerFitzroy <snowferretfitzroy85@gmail.com>
2026-08-27 11:13:58 -0500

    PLEASEJUSTWORKALREADYAAAAAAAAAAA

--- code/fire_dislikes.lua
+++ code/fire_dislikes.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 local sfxManager = SFXManager()
 
 function MUSTELIDMOD:noPickUpFire(player, flag)
@@ -10,17 +9,4 @@ function MUSTELIDMOD:noPickUpFire(player, flag)
 	end
 end
 
-=======
-local sfxManager = SFXManager()
-
-function MUSTELIDMOD:noPickUpFire(player, flag)
-	if player:GetPlayerType() == MUSTELIDMOD_CHARACTERS.FIRE then
-	    if player:HasCollectible(CollectibleType.COLLECTIBLE_SAUSAGE) or player:HasCollectible(CollectibleType.COLLECTIBLE_EPIC_FETUS) or player:HasCollectible(CollectibleType.COLLECTIBLE_DR_FETUS) or player:HasCollectible(CollectibleType.COLLECTIBLE_FREEZER_BABY) or player:HasCollectible(CollectibleType.COLLECTIBLE_CUBE_BABY) or player:HasCollectible(CollectibleType.COLLECTIBLE_URANUS) or player:HasCollectible(CollectibleType.COLLECTIBLE_NEPTUNUS) or player:HasCollectible(CollectibleType.COLLECTIBLE_HOLY_WATER) or player:HasCollectible(CollectibleType.COLLECTIBLE_AQUARIUS) or player:HasCollectible(CollectibleType.COLLECTIBLE_ISAACS_TEARS) or player:HasCollectible(CollectibleType.COLLECTIBLE_TRISAGION) or player:HasCollectible(CollectibleType.COLLECTIBLE_FREE_LEMONADE) then
-			SFXManager():Play(SoundEffect.SOUND_SATAN_GROW, 1, 0, false, 1, 0)
-			player:AddBrokenHearts(666)
-		end
-	end
-end
-
->>>>>>> origin
 MUSTELIDMOD:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, MUSTELIDMOD.noPickUpFire)
--- code/socket_dislikes.lua
+++ code/socket_dislikes.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 local sfxManager = SFXManager()
 
 function MUSTELIDMOD:noPickUpSocket(player, flag)
@@ -10,17 +9,4 @@ function MUSTELIDMOD:noPickUpSocket(player, flag)
 	end
 end
 
-=======
-local sfxManager = SFXManager()
-
-function MUSTELIDMOD:noPickUpSocket(player, flag)
-	if player:GetPlayerType() == MUSTELIDMOD_CHARACTERS.SOCKET then
-	    if player:HasCollectible(CollectibleType.COLLECTIBLE_DEPRESSION) or player:HasCollectible(CollectibleType.COLLECTIBLE_EUCHARIST) or player:HasCollectible(CollectibleType.COLLECTIBLE_INFESTATION_2) or player:HasCollectible(CollectibleType.COLLECTIBLE_SPIDER_BUTT) or player:HasCollectible(CollectibleType.COLLECTIBLE_BEST_BUD) or player:HasCollectible(CollectibleType.COLLECTIBLE_GODHEAD) or player:HasCollectible(CollectibleType.COLLECTIBLE_IMMACULATE_HEART) then
-			SFXManager():Play(SoundEffect.SOUND_SATAN_GROW, 1, 0, false, 1, 0)
-			player:AddBrokenHearts(666)
-		end
-	end
-end
-
->>>>>>> origin
 MUSTELIDMOD:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, MUSTELIDMOD.noPickUpSocket)
